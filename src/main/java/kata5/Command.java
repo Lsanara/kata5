4623581b11e8c4459456379012d25b0a30a7fbc0
Lsanara <nebeproyects@gmail.com>
2024-06-30 04:29:43 +0100

    Second commit

--- src/main/java/kata5/Command.java
+++ src/main/java/kata5/Command.java
@@ -1,2 +1,12 @@
-package kata5;public interface Command {
+package kata5;
+
+public interface Command {
+    Output execute(Input input);
+    interface Input{
+        String get(String key);
+    }
+    interface Output{
+        String result ();
+        int response();
+    }
 }
